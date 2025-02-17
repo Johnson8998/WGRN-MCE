1132e50ddba5b0f7fed7cdea75bd044ce51dccb2
Johnson8998 <93464793+Johnson8998@users.noreply.github.com>
2025-02-17 16:47:43 +0800

    Update MCE_gpu.m

--- MCE_gpu.m
+++ MCE_gpu.m
@@ -1,66 +1,70 @@
-
-function [ge, p_max] = MCE_gpu(data_matrix, net_matrix)
+function ge = MCE(data, net)
 %Input: 
 % data: gene expression dataset, p*n where p is the number of genes and n 
 %   is the number of samples.
 % net: p*p adjacency matrix of gene network (usually PPI). The diagnal is 1 NOT 0!!!
 %Output:
 % ge: a length n vector, ge(i) is the MCE of sample i.
-% % % data = load('rnatest2.csv');
-% % % net = load('adj_matrix.csv');
 
-data_gpu = gpuArray(data_matrix);
-[p,n] = size(data_gpu);
+[p,n] = size(data);
+net(logical(speye(size(net)))) = 1;
+[r, c] = find(net);
+nx = nnz(net);
 
-% net_gpu = net_gpu + speye(size(net_gpu));
-net_matrix(logical(speye(size(net_matrix)))) = 1;
-% net_gpu = sparse(net_gpu);%19892*19892
-net_gpu = gpuArray(net_matrix);
-[r, c] = find(net_gpu);
-nx = nnz(net_gpu);
-
-ge_gpu = gpuArray(zeros(1, n));
-% p_max_gpu = cell(2,n);
-p_max = cell(2,n);
+ge = zeros(1, n);
 
 for i = 1:n
-    p0 = data_gpu(:, i); p0 = p0/sum(p0);
-    % p0 = full(p0);
+    p0 = data(:, i); p0 = p0/sum(p0);
     B = sparse(r, c, p0(r), p, p);
-    % tic
-    [out_gpu, err_gpu, nstep] = Code_5_1_fun_iteration_gpu(net_gpu, B, p0);
-    % toc
+    [out, err, nstep] = fun_iteration(net, B, p0);
     
-    ge_gpu(i) = - dot(p0, log(out_gpu(1:p).*out_gpu((p+1):end)));
-    %ge_gpu(i) = - dot(p0,log(complex(out_gpu(1:p)).*complex(out_gpu((p+1):end))));%有时候会提醒gpu计算需要变成complex
-    ge_gpu(i) = ge_gpu(i) - sum(p0(p0>0).*log(p0(p0>0)));
-
-    % p_max_gpu = out_gpu(1:p) * out_gpu((p+1):end)' .* net_gpu; %Out of memory on device.
-
-    % f = fopen('p_max.xls','w');
-    % fprintf(f,'%s','source');fprintf(f,'\t');
-    % fprintf(f,'%s','target');fprintf(f,'\t');
-    % fprintf(f,'%s','weight');fprintf(f,'\r\n');
-    % for j = 1:nx
-    %     fprintf(f,'%d',r(j));fprintf(f,'\t');
-    %     fprintf(f,'%d',c(j));fprintf(f,'\t');
-    %     fprintf(f,'%f',out_gpu(r(j))*out_gpu(p+c(j)));fprintf(f,'\r\n');
-    % end
-    % fclose(f)
-
-    % p_max_gpu{1,i} = out_gpu(1:p);
-    % p_max_gpu{2,i} = out_gpu((p+1):end);
-
-    p_max(:,i) = {gather(out_gpu(1:p));gather(out_gpu((p+1):end))};
-    % p_max{1,i} = gather(out_gpu(1:p));
-    % p_max{2,i} = gather(out_gpu((p+1):end));
+    ge(i) = - dot(p0, log(out(1:p).*out((p+1):end)));
     
+    ge(i) = ge(i) - sum(p0(p0>0).*log(p0(p0>0)));
     fprintf('Sample %d/%d: %d iterations with entropy %f, error %f.\n',...
-        i, n, nstep, ge_gpu(i), err_gpu);
+        i, n, nstep, ge(i), err);
 end
 maxMCE = log(nx); %normalization
-ge_gpu = ge_gpu/maxMCE;
-ge = gather(ge_gpu);
+ge = ge/maxMCE;
 end
 
 
+%Iteration to solve nonlinear Lagrangian multipliers equations.
+
+function [out, err, n_it] = fun_iteration(A, B, p0)
+%Input:
+% r,c the nonzeros elements of network, [r,c]=find(net)
+% p: the number of genes
+% p0: the steady state distribution
+%
+%Output:
+% when |x-x0| < epsilon, return x
+% n_it is the count of iteration steps.
+%
+%Algorithm:
+% x = (lambda; theta): length 2p vector.
+% lambda0: length p vector, Lagrangian of \sum_j p_ij = 1, i=1:p
+% theta0: length p vector, theta0+1 is Lagrangian of \sum_i pi_i*p_ij = pi_j, j=1:
+% The transition probability p_ij = exp(theta0_j+lambda0_i/pi_i)
+% theta_i = exp(theta0_i), lambda_i = exp(lambda0_i/p_i);
+% p_ij = theta_j*lambda_i.
+
+
+epsilon = 10^-2; Max_n = 10^6;
+stop = 0; n_it = 0;
+lambda0 = p0;
+theta0 = ones(size(p0));
+while(~stop)
+    n_it = n_it+1;
+    
+    lambda = 1./(A*theta0);
+%     lambda = 1./(A*theta);
+%     theta = p0./(B'*lambda0);
+    theta = p0./(B'*lambda);
+    
+    err = norm([lambda-lambda0; theta-theta0], Inf);
+    stop = ( (err < epsilon) | (n_it>Max_n) );
+    lambda0 = lambda; theta0 = theta;
+end
+out = [lambda; theta];
+end
